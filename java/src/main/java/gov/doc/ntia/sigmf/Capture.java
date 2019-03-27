d6571500474ed586bc4f610f240beb4ed8b9f59c
dboulware <dboulware@ntia.doc.gov>
2019-03-27 14:31:57 -0600

    Made Capture not abstract.

--- java/src/main/java/gov/doc/ntia/sigmf/Capture.java
+++ java/src/main/java/gov/doc/ntia/sigmf/Capture.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public abstract class Capture {
+public class Capture {
 
 
     @JsonProperty("core:sample-start")
