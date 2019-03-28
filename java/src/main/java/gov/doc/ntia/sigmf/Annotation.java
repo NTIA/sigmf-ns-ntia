1c159f2600861146325714330dadef656b761c5f
dboulware <dboulware@ntia.doc.gov>
2019-03-28 15:48:00 -0600

    Made Annotation not Abstract

--- java/src/main/java/gov/doc/ntia/sigmf/Annotation.java
+++ java/src/main/java/gov/doc/ntia/sigmf/Annotation.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.io.Serializable;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public abstract class Annotation implements Serializable {
+public class Annotation implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
