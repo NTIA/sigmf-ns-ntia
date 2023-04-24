a4f76a8150c86791d668094736a95c0e979bebe9
Anthony Romaniello <aromaniello@ntia.gov>
2023-04-24 12:11:19 -0400

    deprecate measurement object, move classification to global

--- java/src/main/java/gov/doc/ntia/sigmf/ext/global/core/Measurement.java
+++ java/src/main/java/gov/doc/ntia/sigmf/ext/global/core/Measurement.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.io.Serializable;
 import java.util.Date;
 
+@Deprecated
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Measurement implements Serializable {
 
