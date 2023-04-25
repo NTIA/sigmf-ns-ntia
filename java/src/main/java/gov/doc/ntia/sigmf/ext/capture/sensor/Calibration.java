f0cbf45f3a217154a127a68b4e24107e10b94e6c
Anthony Romaniello <aromaniello@ntia.gov>
2023-04-25 13:02:28 -0400

    add missing calibration fields to java

--- java/src/main/java/gov/doc/ntia/sigmf/ext/capture/sensor/Calibration.java
+++ java/src/main/java/gov/doc/ntia/sigmf/ext/capture/sensor/Calibration.java
@@ -18,6 +18,19 @@ public class Calibration {
   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSX")
   protected Date datetime;
 
+  @JsonProperty(value = "1db_compression_point", required = false)
+  protected Double compressionPoint;
+
+  protected Double enbw;
+
+  @JsonProperty(value = "mean_noise_power", required = false)
+  protected Double meanNoisePower;
+
+  @JsonProperty(value = "mean_noise_power_units", required = false)
+  protected String meanNoisePowerUnits;
+
+  protected String reference;
+
   public Double getGain() {
     return gain;
   }
@@ -48,5 +61,45 @@ public class Calibration {
 
   public void setDatetime(Date datetime) {
     this.datetime = datetime;
+  }
+
+  public Double getCompressionPoint() {
+    return compressionPoint;
+  }
+
+  public void setCompressionPoint(Double compressionPoint) {
+    this.compressionPoint = compressionPoint;
+  }
+
+  public Double getEnbw() {
+    return enbw;
+  }
+
+  public void setEnbw(Double enbw) {
+    this.enbw = enbw;
+  }
+
+  public Double getMeanNoisePower() {
+    return meanNoisePower;
+  }
+
+  public void setMeanNoisePower(Double meanNoisePower) {
+    this.meanNoisePower = meanNoisePower;
+  }
+
+  public String getMeanNoisePowerUnits() {
+    return meanNoisePowerUnits;
+  }
+
+  public void setMeanNoisePowerUnits(String meanNoisePowerUnits) {
+    this.meanNoisePowerUnits = meanNoisePowerUnits;
+  }
+
+  public String getReference() {
+    return reference;
+  }
+
+  public void setReference(String reference) {
+    this.reference = reference;
   }
 }
